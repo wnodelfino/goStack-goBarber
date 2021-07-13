fb98039f66a6ec2c9de01e2adcc5548c9d6c79d1
Wellynton Delfino <wnodelfino@gmail.com>
2021-07-13 00:08:40 -0300

    Estrutura de Toasts

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
-      <GlobalStyle />
     </AuthProvider>
+
+    <ToastContainer />
+
+    <GlobalStyle />
   </>
 );
 
